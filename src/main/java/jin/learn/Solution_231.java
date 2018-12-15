14e1b98bc9bcc27e414f27b4672b2cfaf0f60c8a
zhangjin <jinzhang_hotline@163.com>
2018-12-15 16:21:26 +0800

    231

--- src/main/java/jin/learn/Solution_231.java
+++ src/main/java/jin/learn/Solution_231.java
@@ -1,19 +1,26 @@
 package jin.learn;
 
 
-import java.util.LinkedList;
-
 public class Solution_231 {
-    char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-    public String toHex(int num) {
-        if (num == 0) return "0";
-        String result = "";
-        while (num != 0) {
-            result = map[(num & 15)] + result;
-            num = (num >>> 4);
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while (n != 0) {
+            if (n == 2 || n == 1) {
+                return true;
+            } else if ((n & 1) == 1) {
+                return false;
+            } else {
+                n = n >>> 1;
+            }
         }
-        return result;
+        return true;
     }
+
+    public static void main(String[] args) {
+        boolean powerOfTwo = new Solution_231().isPowerOfTwo(4);
+        System.out.println(powerOfTwo);
+
+    }
+
 }
 
