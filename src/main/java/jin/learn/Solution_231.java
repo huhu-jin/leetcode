8fb322537150bd7acafde3adb9ec06fabcc0d931
zhangjin <jinzhang_hotline@163.com>
2018-12-15 15:59:10 +0800

    231

---
+++ src/main/java/jin/learn/Solution_231.java
@@ -0,0 +1,19 @@
+package jin.learn;
+
+
+import java.util.LinkedList;
+
+public class Solution_231 {
+    char[] map = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    public String toHex(int num) {
+        if (num == 0) return "0";
+        String result = "";
+        while (num != 0) {
+            result = map[(num & 15)] + result;
+            num = (num >>> 4);
+        }
+        return result;
+    }
+}
+
