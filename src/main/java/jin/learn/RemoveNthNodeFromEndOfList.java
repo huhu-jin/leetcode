e2fcd1800fc478c6426d51f1cc2513d5ba1455e2
jinzh3 <jinzh3@cisco.com>
2019-12-08 15:32:02 +0800

    done 19

---
+++ src/main/java/jin/learn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,35 @@
+package jin.learn;
+
+// 思路: 两个指针 快慢指针.快的比慢的差n个位置, 快的到底了, 慢的就是想要的位置
+
+public class RemoveNthNodeFromEndOfList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
