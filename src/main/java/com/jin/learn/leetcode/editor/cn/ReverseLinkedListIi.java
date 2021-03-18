e28a18064d18dc11cf08f7454314db36b60137b7
John Zhang <jinzh3@cisco.com>
2021-03-18 15:16:11 +0800

    daily commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/ReverseLinkedListIi.java
@@ -0,0 +1,98 @@
+//  [92]反转链表 II
+
+//给你单链表的头节点 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链
+//表节点，返回 反转后的链表 。
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], left = 2, right = 4
+//输出：[1,4,3,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [5], left = 1, right = 1
+//输出：[5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目为 n 
+// 1 <= n <= 500 
+// -500 <= Node.val <= 500 
+// 1 <= left <= right <= n 
+// 
+//
+// 
+//
+// 进阶： 你可以使用一趟扫描完成反转吗？ 
+// Related Topics 链表 
+// 👍 747 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import com.jin.learn.leetcode.editor.cn.common.ListNode;
+import org.testng.annotations.Test;
+
+
+public class ReverseLinkedListIi{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        new Solution().reverseBetween(listNode1,1,2);
+
+//        new Solution().reverseBetween(listNode5,1,1);
+    }
+  
+}
