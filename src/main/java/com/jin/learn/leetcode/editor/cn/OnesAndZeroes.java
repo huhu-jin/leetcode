eb34b7154bc5c10af5d7eea1db74cbaa59586715
John Zhang <jinzh3@cisco.com>
2021-03-26 11:30:23 +0800

    daily commit

--- src/main/java/com/jin/learn/leetcode/editor/cn/OnesAndZeroes.java
+++ src/main/java/com/jin/learn/leetcode/editor/cn/OnesAndZeroes.java
@@ -53,36 +53,10 @@ public class OnesAndZeroes{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
         public int findMaxForm(String[] strs, int m, int n) {
-            int strsNum = strs.length;
-            int[][][] dp = new int[strsNum + 1][m + 1][n + 1];
-
-
-            for (int i = 1; i <= strsNum; i++) {
-                int[] cnt = count(strs[i - 1]);
-
-                for (int j = 0; j <= m; j++) {
-                    for(int k = 0; k <= n; k++) {
-                        if (cnt[0] > j || cnt[1] > k) {
-                            dp[i][j][k] = dp[i - 1][j][k];
-                        } else {
-                            dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i - 1][j - cnt[0]][k - cnt[1]] + 1);
-                        }
-                    }
-                }
-            }
-
-            return dp[strsNum][m][n];
+                return 0;
         }
 
 
-        // cnt[0] = zeroNums, cnt[1] = oneNums
-        public int[] count(String str) {
-            int[] cnt = new int[2];
-            for (char c : str.toCharArray()) {
-                cnt[c - '0']++;
-            }
-            return cnt;
-        }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
