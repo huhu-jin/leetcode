ab215d9922560749ce1243401570fee1cd1e2349
zhangjin <jinzhang_hotline@163.com>
2021-05-11 15:50:08 +0800

    linkedNode commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,99 @@
+//  [19]删除链表的倒数第 N 个结点
+
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+// Related Topics 链表 双指针 
+// 👍 1352 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import com.jin.learn.leetcode.editor.cn.common.ListNode;
+import org.testng.annotations.Test;
+
+
+public class RemoveNthNodeFromEndOfList{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if(head == null|| head.next == null) return null;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (--n > 0) {
+            fast = fast.next;
+        }
+        if(fast.next == null){
+            dummy.next = dummy.next.next;
+            return dummy.next;
+        }
+         while (fast.next.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        
+    }
+  
+}
---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/ReverseNodesInKGroup.java
@@ -0,0 +1,131 @@
+//  [25]K 个一组翻转链表
+
+//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 
+//
+// k 是一个正整数，它的值小于或等于链表的长度。 
+//
+// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
+//
+// 进阶： 
+//
+// 
+// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？ 
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 1
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：head = [1], k = 1
+//输出：[1]
+// 
+//
+// 
+// 
+//
+// 提示： 
+//
+// 
+// 列表中节点的数量在范围 sz 内 
+// 1 <= sz <= 5000 
+// 0 <= Node.val <= 1000 
+// 1 <= k <= sz 
+// 
+// Related Topics 链表 
+// 👍 1090 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import com.jin.learn.leetcode.editor.cn.common.ListNode;
+import org.testng.annotations.Test;
+
+
+public class ReverseNodesInKGroup{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        if(head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pre =dummy;
+        while (head !=null) {
+            for (int i = 1; i < k; i++) {
+                if(head.next == null) return dummy.next;
+                head = head.next;
+            }
+            pre = reverse(pre, k);
+            head = pre.next;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode pre, int k) {
+
+        ListNode p = pre.next;
+        for (int i = 1; i < k; i++) {
+            ListNode move = p.next;
+            p.next = move.next;
+            move.next = pre.next;
+            pre.next = move;
+        }
+        return p;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2= new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        new ReverseNodesInKGroup.Solution().reverseKGroup(listNode1, 2);
+    }
+  
+}
