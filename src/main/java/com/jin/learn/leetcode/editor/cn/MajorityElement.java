57f25dada16fce00f2d344f718d042a977e86ab7
John Zhang <jinzh3@cisco.com>
2021-07-02 23:05:28 +0800

    daily commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/MajorityElement.java
@@ -0,0 +1,65 @@
+//  [169]多数元素
+
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[3,2,3]
+//输出：3 
+//
+// 示例 2： 
+//
+// 
+//输入：[2,2,1,1,1,2,2]
+//输出：2
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。 
+// 
+// Related Topics 数组 哈希表 分治 计数 排序 
+// 👍 1044 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import org.testng.annotations.Test;
+
+
+public class MajorityElement{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int x =0;
+        int count=0;
+        for (int i : nums) {
+            if (count == 0)  x = i;
+            if (x == i) {
+                count++;
+            }else {
+                count--;
+            }
+        }
+        return x;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        
+    }
+  
+}
