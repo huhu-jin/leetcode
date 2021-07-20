1d3b09480be05bd5ab9e9eda46d575120bb8d978
John Zhang <jinzh3@cisco.com>
2021-07-20 16:04:01 +0800

    daily commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,99 @@
+//  [98]验证二叉搜索树
+
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。 
+//
+// 假设一个二叉搜索树具有如下特征： 
+//
+// 
+// 节点的左子树只包含小于当前节点的数。 
+// 节点的右子树只包含大于当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 
+// 👍 1133 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import com.jin.learn.leetcode.editor.cn.common.TreeNode;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class ValidateBinarySearchTree{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // left < root< right
+
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        dfs(root);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void dfs(TreeNode root) {
+        if(root == null) return;
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        
+    }
+  
+}
