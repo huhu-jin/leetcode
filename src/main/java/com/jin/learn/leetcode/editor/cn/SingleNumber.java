7621911d5a50b014d83daf6d2ecebac7b9278da0
John Zhang <jinzh3@cisco.com>
2021-07-21 14:47:41 +0800

    daily commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/SingleNumber.java
@@ -0,0 +1,51 @@
+//  [136]只出现一次的数字
+
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 数组 
+// 👍 1927 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import org.testng.annotations.Test;
+
+
+public class SingleNumber{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums == null) return 0;
+        if(nums.length <=1) return nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        
+    }
+  
+}
