6dc070aa3b8d40f0125f63eb57976e0ea9c976bc
John Zhang <jinzh3@cisco.com>
2022-06-15 22:06:14 +0800

    commit daily

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/LengthOfLastWord.java
@@ -0,0 +1,79 @@
+//  [58]最后一个单词的长度
+
+//给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。 
+//
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "Hello World"
+//输出：5
+//解释：最后一个单词是“World”，长度为5。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "   fly me   to   the moon  "
+//输出：4
+//解释：最后一个单词是“moon”，长度为4。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "luffy is still joyboy"
+//输出：6
+//解释：最后一个单词是长度为6的“joyboy”。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s 仅有英文字母和空格 ' ' 组成 
+// s 中至少存在一个单词 
+// 
+// Related Topics 字符串 👍 465 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+// 不用看
+public class LengthOfLastWord{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int i = s.length()-1;
+        for (; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+        }
+        return s.length()-1-i;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        int i = new Solution().lengthOfLastWord("Hello World");
+        Assert.assertEquals(i,5);
+
+         i = new Solution().lengthOfLastWord("   fly me   to   the moon  ");
+        Assert.assertEquals(i,4);
+    }
+  
+}
