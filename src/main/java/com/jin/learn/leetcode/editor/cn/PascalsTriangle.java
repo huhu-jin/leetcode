0152d17428a81800f3f997e7a7a98fa7c1e91b97
John Zhang <jinzh3@cisco.com>
2022-07-03 22:28:43 +0800

    commit daily

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/PascalsTriangle.java
@@ -0,0 +1,82 @@
+//  [118]杨辉三角
+
+//给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。 
+//
+// 在「杨辉三角」中，每个数是它左上方和右上方的数的和。 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: numRows = 5
+//输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: numRows = 1
+//输出: [[1]]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= numRows <= 30 
+// 
+// Related Topics 数组 动态规划 👍 782 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import org.testng.annotations.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class PascalsTriangle{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+    // 不用看
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new LinkedList<>();
+
+        List<Integer> root = new LinkedList<>();
+        root.add(1);
+        ans.add(root);
+
+        for (int count = 2; count <= numRows; count++) {
+            List<Integer> pre = ans.get(count - 2);
+            List <Integer> temp = new LinkedList<>();
+            int i =1;
+            temp.add(1);
+            while (i < count-1) {
+                temp.add(pre.get(i - 1) + pre.get(i));
+                i++;
+            }
+            temp.add(1);
+            ans.add(temp);
+        }
+
+
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        new Solution().generate(5);
+        
+    }
+  
+}
