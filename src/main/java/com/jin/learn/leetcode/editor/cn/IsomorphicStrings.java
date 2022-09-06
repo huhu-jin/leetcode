9c31893b1004e9e8ff6234467ebf47d41a61b161
jin <jinzhang_hotline@163.com>
2022-09-06 23:04:49 +0800

    daily commit

---
+++ src/main/java/com/jin/learn/leetcode/editor/cn/IsomorphicStrings.java
@@ -0,0 +1,93 @@
+//  [205]同构字符串
+
+//给定两个字符串 s 和 t ，判断它们是否是同构的。 
+//
+// 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。 
+//
+// 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入：s = "egg", t = "add"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "foo", t = "bar"
+//输出：false 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "paper", t = "title"
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+//
+// 
+// 1 <= s.length <= 5 * 10⁴ 
+// t.length == s.length 
+// s 和 t 由任意有效的 ASCII 字符组成 
+// 
+// Related Topics 哈希表 字符串 👍 512 👎 0
+
+
+package com.jin.learn.leetcode.editor.cn;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+
+public class IsomorphicStrings{
+  
+  
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // 题目很奇怪
+    //  paper, title ture
+    // e->l , r-> e
+    // l-> e , e ->
+     // 两个 e 不一样
+    public boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character, Character> s2t = new HashMap<>();
+        HashMap<Character, Character> t2s = new HashMap<>();
+
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+
+            char x = s.charAt(i);
+            char y = t.charAt(i);
+
+            if (s2t.containsKey(x) && s2t.get(x) != y || t2s.containsKey(y) && t2s.get(y) != x) {
+                return false;
+            }
+            s2t.put(x, y);
+            t2s.put(y, x);
+        }
+
+
+        return true;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    @Test
+    public void testCase(){
+        
+    }
+  
+}
